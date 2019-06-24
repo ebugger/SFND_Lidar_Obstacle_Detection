2a00a4145a70d31855210f51701ad010659baa4f
Younger <youngerliu@gmail.com>
2019-06-24 19:58:57 +0800

    bug fix for  if/else

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -25,19 +25,19 @@ struct KdTree
 	: root(NULL)
 	{}
 
-	void insert_helper(Node** node, int depth, std::vector<float> point, int id) {
+	void insert_helper(Node** node, uint depth, std::vector<float> point, int id) {
 		if(*node == NULL) {
 			*node = new Node(point, id);
-		}
-
-		uint cd = depth % 2;
-
-		if(point[cd] < ((*node)->point[cd])) {
-			//depth++;
-			insert_helper(&((*node)->left), depth + 1, point, id);
 		}else {
-			//depth++;
-			insert_helper(&((*node)->right), depth + 1, point, id);
+			uint cd = depth % 2;
+
+			if(point[cd] < ((*node)->point[cd])) {
+				//depth++;
+				insert_helper(&((*node)->left), depth + 1, point, id);
+			}else {
+				//depth++;
+				insert_helper(&((*node)->right), depth + 1, point, id);
+			}
 		}
 	}
 
