7a58c1e0f76beb6498cf4181fdbbbb8238d5fe74
Younger <youngerliu@gmail.com>
2019-07-02 17:29:07 +0800

    update the clustering paras for city block

--- src/environment.cpp
+++ src/environment.cpp
@@ -73,7 +73,7 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
     for(pcl::PointCloud<pcl::PointXYZ>::Ptr cluster : cloudClusters) {
         std::cout << "cluster size ";
         processor_PC.numPoints(cluster);
-        renderPointCloud(viewer,cluster,"obstCloud"+std::to_string(clusterId),colors[clusterId]);
+        renderPointCloud(viewer,cluster,"obstCloud"+std::to_string(clusterId),colors[clusterId%3]);
         //using bbox
         Box box = processor_PC.BoundingBox(cluster);
         renderBox(viewer, box, clusterId);
@@ -90,14 +90,14 @@ void cityBlock(pcl::visualization::PCLVisualizer::Ptr& viewer) {
     pcl::PointCloud<pcl::PointXYZI>::Ptr inputcloud = pointProcessorI->loadPcd("../src/sensors/data/pcd/data_1/0000000000.pcd");
     //renderPointCloud(viewer, inputcloud, "inputcloud" );
     //from topdown view, up for positive x, left for positive y, and the origin for z is on the top of the car.
-    filter_cloud = pointProcessorI->FilterCloud(inputcloud, 0.25f, Eigen::Vector4f (-6, -4, -2, 1), Eigen::Vector4f (13, 7, 1, 1));
+    filter_cloud = pointProcessorI->FilterCloud(inputcloud, 0.25f, Eigen::Vector4f (-6, -4, -2, 1), Eigen::Vector4f (32, 7, 1, 1));
     //renderPointCloud(viewer, filter_cloud, "filterCould");
     std::pair<pcl::PointCloud<pcl::PointXYZI>::Ptr, pcl::PointCloud<pcl::PointXYZI>::Ptr> my_seg_cloud = pointProcessorI->SegmentPlane(filter_cloud, 100, 0.2);
     //Color(R,G,B)
-    renderPointCloud(viewer, my_seg_cloud.first, "obstacle_cloud", Color(1,0,0));
+    //renderPointCloud(viewer, my_seg_cloud.first, "obstacle_cloud", Color(1,0,0));
     renderPointCloud(viewer, my_seg_cloud.second, "plnae_cloud", Color(0,1,0));
-
-    std::vector<pcl::PointCloud<pcl::PointXYZI>::Ptr> cloudClusters = pointProcessorI->Clustering(my_seg_cloud.first, 1.0, 3, 30);
+    //Max is so important as big objects always contains much more points.
+    std::vector<pcl::PointCloud<pcl::PointXYZI>::Ptr> cloudClusters = pointProcessorI->Clustering(my_seg_cloud.first, 0.4, 100, 900);
     int clusterId = 0;
     //yellow for (1,1,0)
     std::vector<Color> colors = {Color(1,0,0), Color(1,1,0), Color(0,0,1)};
