139daefc158834c1cd16da8f5bc445118a5001bb
Younger Liu <youngerliu@hotmail.com>
2019-07-04 11:54:04 +0800

    single pcd with rewrite clustering

--- src/environment.cpp
+++ src/environment.cpp
@@ -7,7 +7,108 @@
 #include "processPointClouds.h"
 // using templates for processPointClouds so also include .cpp to help linker
 #include "processPointClouds.cpp"
+#include "./render/render.h"
+#include "./render/box.h"
+#include <chrono>
+#include <string>
+#include "./quiz/cluster/kdtree.h"
+#include <unordered_set>
+Box BoundingBox(pcl::PointCloud<pcl::PointXYZI>::Ptr cluster)
+{
 
+    // Find bounding box for one of the clusters
+    pcl::PointXYZI minPoint;
+	pcl::PointXYZI maxPoint;
+    pcl::getMinMax3D(*cluster, minPoint, maxPoint);
+	
+
+    Box box;
+    box.x_min = minPoint.x;
+    box.y_min = minPoint.y;
+    box.z_min = minPoint.z;
+    box.x_max = maxPoint.x;
+    box.y_max = maxPoint.y;
+    box.z_max = maxPoint.z;
+
+    return box;
+}
+
+void Proximity(const std::vector<std::vector<float>>& points, std::vector<int>& cluster, int idx, std::unordered_set<int> &process_list,  KdTree* tree, float distanceTol){
+	std::vector<int> nearby_points;
+	process_list.insert(idx);
+	cluster.push_back(idx);
+	nearby_points = tree->search(points[idx], distanceTol);
+	for(int nearby_id : nearby_points) {
+		if(process_list.count(nearby_id) == 0){
+			Proximity(points, cluster, nearby_id, process_list, tree, distanceTol);
+		}
+	}
+
+}
+
+std::vector<pcl::PointCloud<pcl::PointXYZI>::Ptr> euclideanCluster( pcl::PointCloud<pcl::PointXYZI>::Ptr cloud, float distanceTol, int minSize, int maxSize)
+{
+	std::vector<pcl::PointCloud<pcl::PointXYZI>::Ptr> clusters_;
+	// TODO: Fill out this function to return list of indices for each cluster
+	std::vector<std::vector<int>> clusters;
+	std::vector<std::vector<float>> points;
+    std::vector< std::vector<float> > points_;
+	//or use std::vector<bool> process_list(points.size(), false) and check if(process_list[i]) in future;
+	std::unordered_set<int> process_list {};
+	std::vector<pcl::PointIndices> cluster_indices;
+    
+    KdTree* tree = new KdTree;
+	
+	//optimaized for binary tree(extract the median element in the dimention)
+	int p_size = cloud->points.size();
+	for (int i=0;i<p_size;i++ ){
+	    std::vector<float> temp;	
+		temp.push_back(cloud->points[i].x);
+		temp.push_back(cloud->points[i].y);
+		temp.push_back(cloud->points[i].z);
+		points.push_back(temp);
+	}
+    for (int i=0; i<p_size; i++) {
+		int idx = i % 3;
+		//get the median element on rolling x->y->z
+        std::nth_element(points.begin(), points.begin() + points.size()/2, points.end(),
+            [idx](std::vector<float> const &lhs,
+             std::vector<float> const &rhs) { return lhs[idx] < rhs[idx]; });
+		tree->insert(points[points.size()/2],i);
+		//reorder the the points by the insert order
+		cloud->points[i].x = points[points.size()/2][0];
+                cloud->points[i].y = points[points.size()/2][1];
+                cloud->points[i].z = points[points.size()/2][2];
+		points_.push_back(points[points.size()/2]);
+		//remove the median(swap to end and popback)
+		iter_swap(points.begin()+points.size()/2,points.end()-1);
+		points.pop_back();		
+	}
+
+	for (int i=0;i<points_.size();i++) {
+		if(process_list.count(i) == 0) {
+			pcl::PointIndices cluster;
+			//std::vector<int> cluster {};
+			Proximity(points_, cluster.indices, i, process_list, tree, distanceTol);
+			if((cluster.indices.size() >= minSize) && (cluster.indices.size() <= maxSize))
+			//	cluster_indices.push_back(cluster);
+			//      cout<<"Proximity done for size:  "<< cluster.indices.size()<<endl;
+				cluster_indices.push_back(cluster);
+		}
+	}
+    for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin (); it != cluster_indices.end (); ++it) {
+        pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_cluster (new pcl::PointCloud<pcl::PointXYZI>);
+        for (std::vector<int>::const_iterator pit = it->indices.begin (); pit != it->indices.end (); ++pit) {
+            cloud_cluster->points.push_back (cloud->points[*pit]);
+        }
+        cloud_cluster->width = cloud_cluster->points.size ();
+        cloud_cluster->height = 1;
+        cloud_cluster->is_dense = true;
+        clusters_.push_back(cloud_cluster);
+    }	
+	return clusters_;
+
+}
 std::vector<Car> initHighway(bool renderScene, pcl::visualization::PCLVisualizer::Ptr& viewer)
 {
 
@@ -99,7 +200,8 @@ void cityBlock(pcl::visualization::PCLVisualizer::Ptr& viewer) {
     //Max is so important as big objects always contains much more points.
     Box roof_box = {-2.6, -1.7, -1, 2.6, 1.7, -.4};
     renderBox(viewer, roof_box, 33265, Color(128,0,128));
-    std::vector<pcl::PointCloud<pcl::PointXYZI>::Ptr> cloudClusters = pointProcessorI->Clustering(my_seg_cloud.first, 0.4, 80, 1000);
+    //std::vector<pcl::PointCloud<pcl::PointXYZI>::Ptr> cloudClusters = pointProcessorI->Clustering(my_seg_cloud.first, 0.4, 80, 1000);
+    std::vector<pcl::PointCloud<pcl::PointXYZI>::Ptr> cloudClusters = euclideanCluster(my_seg_cloud.first, 0.4, 80, 1000);
     int clusterId = 0;
     //yellow for (1,1,0)
     std::vector<Color> colors = {Color(1,0,0), Color(1,1,0), Color(0,0,1)};
@@ -108,40 +210,8 @@ void cityBlock(pcl::visualization::PCLVisualizer::Ptr& viewer) {
         pointProcessorI->numPoints(cluster);
         renderPointCloud(viewer,cluster,"obstCloud"+std::to_string(clusterId),colors[clusterId%3]);
         //using bbox
-        Box box = pointProcessorI->BoundingBox(cluster);
-        renderBox(viewer, box, clusterId);
-        ++clusterId;
-    }
-
-}
-
-void newcityBlock(pcl::visualization::PCLVisualizer::Ptr& viewer, ProcessPointClouds<pcl::PointXYZI>* pointProcessorI, const pcl::PointCloud<pcl::PointXYZI>::Ptr& inputcloud) {
-    bool renderScene = false;
-    std::vector<Car> cars = initHighway(renderScene, viewer);
-    pcl::PointCloud<pcl::PointXYZI>::Ptr filter_cloud (new pcl::PointCloud<pcl::PointXYZI> ());
-
-    //renderPointCloud(viewer, inputcloud, "inputcloud" );
-    //from topdown view, up for positive x, left for positive y, and the origin for z is on the top of the car.
-    filter_cloud = pointProcessorI->FilterCloud(inputcloud, 0.15f, Eigen::Vector4f (-10, -4, -2, 1), Eigen::Vector4f (32, 7, 1, 1));
-    //renderPointCloud(viewer, filter_cloud, "filterCould");
-    std::pair<pcl::PointCloud<pcl::PointXYZI>::Ptr, pcl::PointCloud<pcl::PointXYZI>::Ptr> my_seg_cloud = pointProcessorI->SegmentPlane(filter_cloud, 100, 0.2);
-    //Color(R,G,B)
-    //renderPointCloud(viewer, my_seg_cloud.first, "obstacle_cloud", Color(1,0,0));
-    renderPointCloud(viewer, my_seg_cloud.second, "plnae_cloud", Color(0,1,0));
-    //Max is so important as big objects always contains much more points.
-    Box roof_box = {-2.6, -1.7, -1, 2.6, 1.7, -.4};
-    renderBox(viewer, roof_box, 33265, Color(128,0,128));
-    std::vector<pcl::PointCloud<pcl::PointXYZI>::Ptr> cloudClusters = pointProcessorI->Clustering(my_seg_cloud.first, 0.4, 80, 1000);
-    int clusterId = 0;
-    //yellow for (1,1,0)
-    std::vector<Color> colors = {Color(1,0,0), Color(1,1,0), Color(0,0,1)};
-    for(pcl::PointCloud<pcl::PointXYZI>::Ptr cluster : cloudClusters) {
-        std::cout << "cluster size ";
-        pointProcessorI->numPoints(cluster);
-        renderPointCloud(viewer,cluster,"obstCloud"+std::to_string(clusterId),colors[clusterId%3]);
-        //using bbox
-        Box box = pointProcessorI->BoundingBox(cluster);
-        renderBox(viewer, box, clusterId);
+        //Box box = pointProcessorI->BoundingBox(cluster);
+        //renderBox(viewer, box, clusterId);
         ++clusterId;
     }
 
@@ -187,22 +257,9 @@ int main (int argc, char** argv)
     //the sensation of being in the car’s driver seat.
     initCamera(setAngle, viewer);
     //impleHighway(viewer);
-    //cityBlock(viewer);
-    ProcessPointClouds<pcl::PointXYZI>* pointProcessorI = new ProcessPointClouds<pcl::PointXYZI> ();
-    std::vector<boost::filesystem::path> stream = pointProcessorI->streamPcd("../src/sensors/data/pcd/data_1");
-    auto streamIterator = stream.begin();
-    pcl::PointCloud<pcl::PointXYZI>::Ptr inputcloudI;
+    cityBlock(viewer);
     while (!viewer->wasStopped ())
     {
-        viewer->removeAllPointClouds();
-        viewer->removeAllShapes();
-        inputcloudI = pointProcessorI->loadPcd((*streamIterator).string());
-        newcityBlock(viewer, pointProcessorI, inputcloudI);
-
-        streamIterator++;
-        if(streamIterator == stream.end())
-            streamIterator = stream.begin();
-
         viewer->spinOnce ();
     } 
 }
